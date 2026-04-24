7e062fd746b4df8d916fce72d37f5cf3cfaeda5f
isra-shai-ai <shai@isra.ai>
2026-04-24 09:45:42 +0300

    style: Align service card links to the right on mobile

--- tmp_find_sections.py
+++
@@ -1,9 +0,0 @@
-import re
-
-with open('public/index.html', 'r', encoding='utf-8') as f:
-    html = f.read()
-
-# Find all section tags and their IDs
-sections = re.findall(r'<section[^>]*>', html, re.IGNORECASE)
-for sec in sections:
-    print(sec)
